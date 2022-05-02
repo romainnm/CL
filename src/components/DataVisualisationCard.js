b96945dc2bec389d22e8b5dec5552c4c034b3d9e
Romain N <insc.romain@gmail.com>
2022-05-02 12:21:18 -0500

    mock image path

--- src/components/DataVisualisationCard.js
+++ src/components/DataVisualisationCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import mockimg from "./mock-img.jpg";
+
 const DataVisualisationCard = () => {
   return (
     <article className="data-card">
       <Link to="/CL/data-visualisation/1" className="data-card__img-container">
         <h3>Project Name</h3>
-        <img src="./images/mock-img.jpg" alt="data-project" />
+        <img src={mockimg} alt="data-project" />
       </Link>
     </article>
   );
